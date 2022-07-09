6de97300e6ce09b9d6bee05eec459b3b32c698ee
Nicola Masilla <nicomasilla@gmail.com>
2022-07-09 20:51:38 +0200

    UPDATE:  changed the data structure created at the end of the script to handle it better when saving it on xlxs

--- targets/fornitures/index.js
+++ targets/fornitures/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import axios from "axios"
 import cheerio from "cheerio";
 import fs from 'fs';
-import {performance} from "perf_hooks";
+
 
 //import URLs_obj from "../node_scraper/data/data.js";
 
@@ -26,78 +26,88 @@ export const main = async () => {
     return regions
   }
 
-  const sendRequest = async (URL, region) => {
+  const sendRequest = async (URL, provincia = "") => {
     // get data from the url using axios and cheerio
     let errors = [];
-    let regionObject = {};
+    let dataFromProvincia = [];
+    let addressMatrix = [];
+    //let formattedProvincia = provincia.replace(' ', '-').capitalize();
     try {
       const resp = await axios(URL);
       const HTML = resp.data // get the html from the response
       const $ = cheerio.load(HTML) // ! init cheerio as $
-      
+
       // get an array of all the titles
       const titles = $('.card__title').toArray().map(item => $(item).text())
       // get an array of all the addresses
       let addresses = $('.card__store-address').toArray().map(item => $(item).text())
       // normalize the addresses array
-      addresses = normalizeAddressesArray(addresses)
-      regionObject[region] = {
-        "names" : titles,
-        "addresses" : addresses
-      }
       //console.log('TITLES', titles);
-      //console.log('ADDRESSES', addresses);
-      //console.log(regionObject);
+      addressMatrix = normalizeAddressesArray(addresses);
+      for (let index = 0; index < titles.length; index++) {
+        dataFromProvincia.push({
+          name: titles[index],
+          indirizzo: addressMatrix[index][0].toUpperCase(),
+          citta: addressMatrix[index][1].replace(" ", "-").toUpperCase(),
+          provincia: provincia,
+        })
+      }
+      //console.log('dataFromProvincia', dataFromProvincia);
     } catch (error) {
-      //console.log('error :::', region);
-      errors.push(region); // add the compromised region to the errors array
+      //console.log('error :::', error);
+      console.log('error :::', `name of ${provincia} not found`);
+      errors.push(provincia); // add the compromised region to the errors array
     }
-    return [regionObject, errors];
+    return [dataFromProvincia, errors];
   }
-  // takes in an array of addresses 
-  // for each address, transform it into an array 
-  // remove useless characters and recombine the array with the strings capitalized
-  const normalizeAddressesArray = (addressesArray) => {
-    let refinedAddressess = [];
-    let finalArray = [];
-    for (const iterator of addressesArray) {
-      let refinedAddress = iterator.replace(' ', '').trim();
-      refinedAddressess.push(refinedAddress);
-    } 
-    for (const iterator of refinedAddressess) {
-      let splittedAddress = iterator.split('\n').map(item => item.trim().toUpperCase());
-      finalArray.push(splittedAddress.join(' '));
+
+  //sendRequest("https://www.venetacucine.com/store-locator/italia/agrigento");
+  const normalizeAddressesArray = (addresses) => {
+    let matrix = [];
+    for (const address of addresses) {
+      let normalizedSingleAddress = address.replace(' ', '').trim().split('\n') // rimuove spazi centrali e inizio fine, poi fa lo split per a capo
+      normalizedSingleAddress.pop() // rimuove l'ultimo elemento dell'array perchè è inutile
+      normalizedSingleAddress[1] = normalizedSingleAddress[1].replace(' ', '').trim() // manipola la seconda stringa nell'array per rimuovere gli spazi
+      let extrapulatedCity = normalizedSingleAddress[1].split(' '); // splitta la stringa basandosi sugli spazi
+      extrapulatedCity.shift() //rimuove l'elemento iniziale dell'array
+      extrapulatedCity.pop() // rimuove l'ultimo elemento dell'array
+      extrapulatedCity = extrapulatedCity.join(' ') // riunisce l'array in una stringa
+      normalizedSingleAddress[1] = extrapulatedCity.trim() // rimuove spazi all'inizio e alla fine
+      matrix.push(normalizedSingleAddress) // aggiunge l'array alla matrice
     }
-    //console.log(finalArray);
-    return finalArray
+    return matrix;
   }
-  
 
   const prepareDataToBeSaved = async () => {
-    const regions = await getRegionsRequest('https://www.venetacucine.com/store-locator/italia');
+    const provincie = await getRegionsRequest('https://www.venetacucine.com/store-locator/italia');
     let scrapedData = [];
     let n = 0;
-    for (const region of regions) {
-      const url = `https://www.venetacucine.com/store-locator/italia/${region}`;
-      const [regionData, errors] = await sendRequest(url, region); 
-      scrapedData.push(regionData); // add the region data to the scrapedData array
+    for (const provincia of provincie) {  // ! temporary sliced array of provincie
+      const url = `https://www.venetacucine.com/store-locator/italia/${provincia}`;
+      const [regionData, errors] = await sendRequest(url, provincia);
+      for (const iterator of regionData) { // region data e' un array di oggetti
+        iterator["provincia"] = `${provincia[0].toUpperCase()}${provincia.slice(1).toLowerCase()}`.replace(' ', '-');
+        scrapedData.push(iterator); // voglio aggiungere ogni oggetto all'array scrapedData
+      }
       n++;
-      console.log('...iteration n:', n); // print the number of iterations
+      console.log(`iteratin n: ${n} on the province ${provincia}`); // print the number of iterations
     }
-    const toBeSaved = { data: scrapedData}
+    const toBeSaved = { data: scrapedData }
     return toBeSaved;
   }
-
   //let a = performance.now();
   //let b = performance.now();
   //console.log(`Time taken: ${(b - a) / 1000} seconds.`);
-  
   const toBeSaved = await prepareDataToBeSaved();
+  console.log("Writing the json file...");
   fs.writeFileSync('./data/scrapedData.json', JSON.stringify(toBeSaved), 'utf8', (err) => {
-    if (err) throw err;
-    else console.log('The file has been saved!');
+    if (err) {
+      throw err;
+    }
+    else {
+      console.log('The file has been saved!');
+    }
   })
-  //console.log(scrapedData);
 }
 
-  
+
