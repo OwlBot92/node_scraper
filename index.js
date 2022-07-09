e686dd68efa704e9c3bce9037e32449cb49d5ea7
Nicola Masilla <nicomasilla@gmail.com>
2022-07-09 20:52:18 +0200

    UPDATE: main function called inside index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import { main as fornituresMain } from "./targets/fornitures/index.js";
+import manipulator from "./manipulateEx.cjs";
 
+const main = async () => {
+  await fornituresMain();
+  console.log("Writing the xlsx file...");
+  manipulator();
+}
 
-fornituresMain();
+main();
+
+// ! note
+// * to avoid the server to restart every time the script is executed
+// * since at the end of the script some files are being updated or subject to changes
+// Inside packed.json, add the following line:
+// * "nodemonConfig": { "ext": "js", "ignore": ["*.test.ts", "data/*"], "delay": "2" },
